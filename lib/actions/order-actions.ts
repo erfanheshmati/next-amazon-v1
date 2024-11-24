3128dfd0a0e0b8359229ec578ba5f20388b5071a
erfan.heshmati <heshmati74@gmail.com>
2024-11-24 18:45:36 +0330

    Fix email order receipt

--- lib/actions/order-actions.ts
+++ lib/actions/order-actions.ts
@@ -252,7 +252,7 @@ export const updateOrderToPaid = async ({
   });
 
   if (!order) throw new Error("Order not found");
-  // if (order.isPaid) throw new Error("Order is already paid");
+  if (order.isPaid) throw new Error("Order is already paid");
 
   await db.transaction(async (tx) => {
     for (const item of order.orderItems) {
@@ -280,7 +280,7 @@ export const updateOrderToPaid = async ({
   if (!updatedOrder) {
     throw new Error("Order not found");
   }
-  sendPurchaseReceipt({ order: updatedOrder });
+  await sendPurchaseReceipt({ order: updatedOrder });
 };
 
 export async function updateOrderToPaidByCOD(orderId: string) {
